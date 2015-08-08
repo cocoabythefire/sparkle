284404496aa3a2021f4b26237318279835780be2
Brittany Tarvin <bntarvin@fadingred.com>
2015-08-07 18:25:52 -0700

    Set up proxy for development.

---
+++ proxy.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+var PORT = process.env.PORT || 8000;
+var PROXY_PORT = process.env.PROXY || 3000;
+
+var proxy = require('http-proxy').createProxyServer({
+  target: 'http://localhost:' + PROXY_PORT,
+  ws: true,
+  changeOrigin: true
+});
+
+proxy.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head);
+});
+
+proxy.on('error', function (e) {
+  console.log('Error proxying to ' + PROXY_PORT);
+  console.log(e);
+});
+
+app.use(morgan('dev'));
+app.use(express.static('app'));
+app.use(function(req, res, next) {
+  if (req.url.match(/^\/api\//)) {
+    return proxy.web(req, res);
+  }
+  else { next(); }
+});
+
+var server = app.listen(PORT, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Development listening at http://%s:%s w/ proxy to %s', host, port, PROXY_PORT);
+
+});
