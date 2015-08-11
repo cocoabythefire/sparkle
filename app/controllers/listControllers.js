29cb1ac0e9a6be62ccf16bbb48b6999474d8af24
Brittany Tarvin <bntarvin@fadingred.com>
2015-08-11 13:15:58 -0700

    working on getting the list view to work right

--- app/controllers/listControllers.js
+++ app/controllers/listControllers.js
@@ -2,12 +2,16 @@
 
 angular.module('sparkleApp.lists', [])
 .controller('ListCtrl', ['$scope', 'List', function($scope, List) {
-  List.query().$promise.then(function(result) {
-    $scope.lists = result.lists;
-  });
-
-  $scope.createList = function(listName) {
-    List.save({ name:listName }).$promise.then(function(result) {
+  $scope.updateLists = function() {
+    List.query().$promise.then(function(result) {
+      $scope.lists = result.lists;
     });
   };
+  $scope.createList = function(listName) {
+    List.save({ name:listName }).$promise.then(function(result) {
+      $scope.updateLists();
+    });
+  };
+
+  $scope.updateLists();
 }]);
