659a85b32c4bf49551023281fc36ea7dbca14146
Brittany Tarvin <bntarvin@fadingred.com>
2015-08-13 15:02:16 -0700

    puts the new token to the cookies on login

--- app/controllers/authControllers.js
+++ app/controllers/authControllers.js
@@ -1,12 +1,26 @@
 'use strict';
 
+var tokenHeaderKey = 'x-glitter-token';
+
 angular.module('sparkleApp.auth', [])
 .controller('LoginCtrl', ['$scope', '$cookies', 'User', function($scope, $cookies, User) {
+  var getCurrentSessionToken = function() {
+    return $cookies.get(tokenHeaderKey);
+  };
+
   $scope.login = function() {
-    User.login({ username: $scope.formUsername, password: $scope.formPassword }).$promise.then(function(result) {
-      $scope.currentToken = result.headers['x-glitter-token'];
-      $scope.formUsername = "";
-      $scope.formPassword = "";
-    });
+    var currentSessionToken = getCurrentSessionToken();
+    if (!currentSessionToken) {
+      User.login({ username: $scope.formUsername, password: $scope.formPassword }).$promise.then(function(result) {
+        $cookies.putObject(tokenHeaderKey, result.headers[tokenHeaderKey]);
+      });
+    } else {
+      //redirect? do nothing?
+      console.log("Already Logged In");
+    }
+
+    //clear the login form
+    $scope.formUsername = "";
+    $scope.formPassword = "";
   };
 }]);
