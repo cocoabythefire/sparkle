ff75dac106f8c7c098e4ca08a8156e7177ce6116
Brittany Tarvin <bntarvin@fadingred.com>
2015-08-16 11:16:02 -0700

    must be logged in to view lists or profile pages, otherwise redirect to login screen

--- app/controllers/listControllers.js
+++ app/controllers/listControllers.js
@@ -2,30 +2,69 @@
 
 var app = angular.module('sparkleApp.lists', []);
 
-app.controller('ListCtrl', ['$scope', '$routeParams', 'List', function($scope, $routeParams, List) {
+app.controller('ListCtrl', ['$scope', '$routeParams', '$location', 'List', function($scope, $routeParams, $location, List) {
+
+  List.query().$promise.then(function(result) {
+    $scope.lists = result.lists;
+  }).catch(function(error) {
+    if (error.status == 401) {
+      console.log('Must login to access lists.');
+    } else {
+      console.log(error);
+    }
+    $location.path('/login');
+  });
+
+  List.get({ id: $routeParams.id}).$promise.then(function(result) {
+    $scope.list = result.list;
+    $scope.listPlaces = result.places;
+  }).catch(function(error) {
+      if (error.status == 401) {
+        console.log('Must login to view a list.');
+      } else {
+        console.log(error);
+      }
+      $location.path('/login');
+  });
 
   $scope.updateLists = function() {
     List.query().$promise.then(function(result) {
       $scope.lists = result.lists;
+    }).catch(function(error) {
+      if (error.status == 401) {
+        console.log('Must login to access this list.');
+      } else {
+        console.log(error);
+      }
+      $location.path('/login');
     });
   };
   $scope.createList = function(listName) {
     List.save({ name: listName }).$promise.then(function(result) {
       $scope.updateLists();
       $scope.list = {};
+    }).catch(function(error) {
+      if (error.status == 401) {
+        console.log('Must login to create a list.');
+      } else {
+        console.log(error);
+      }
+      $location.path('/login');
     });
   };
 
   $scope.deleteList = function(listId) {
     List.delete({ id: listId }).$promise.then(function(result) {
       $scope.updateLists();
+    }).catch(function(error) {
+      if (error.status == 401) {
+        console.log('Must login to delete a list.');
+      } else {
+        console.log(error);
+      }
+      $location.path('/login');
     });
   };
-
-  List.get({ id: $routeParams.id}).$promise.then(function(result) {
-    $scope.list = result.list;
-    $scope.listPlaces = result.places;
-  });
 
   $scope.updateLists();
 }]);
--- app/controllers/profileControllers.js
+++ app/controllers/profileControllers.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('sparkleApp.profile', [])
-.controller('ProfileViewCtrl', ['$scope', 'User', function($scope, User) {
+.controller('ProfileViewCtrl', ['$scope', 'User', '$location', function($scope, User, $location) {
   $scope.firstName = '';
 
   User.getProfile().$promise.then(function(result) {
     $scope.firstName = result.name;
     $scope.currentUser = result;
+  })
+  .catch(function(error) {
+    if (error.status == 401) {
+      console.log('Must login to access profile');
+    } else {
+      console.log(error);
+    }
+    $location.path('/login');
   });
 }]);
